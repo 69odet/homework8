5fda1f59a2830b516fb6db2da64bfe358e694a59
69odet <odet.o@yandex.ru>
2024-06-09 12:41:59 +0300

    Update module2.3.py

--- module2.3.py
+++ module2.3.py
@@ -1,13 +1,9 @@
 my_list = [47, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
-while i < len(my_list):
-    if my_list[i] >= 0:
-        if my_list[i] != 0:
-            print(my_list[i])
-            i = i+1
-            continue
-        else:
-            print(my_list[i])
-            i = i+1
+while my_list[i] >= 0 and i < len(my_list):
+    if my_list[i] == 0:
+        i += 1
+        continue
+    else:
         print(my_list[i])
-        i = i + 1
+        i += 1
